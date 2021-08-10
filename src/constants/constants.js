76c439986280aa2d153a7a8afb6a62ac7999177e
KingRain <test.gmail.com>
2021-08-10 14:31:52 +0530

    Added another project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: 'Doge',
-    description: "A Multi Purpose Discord Bt made using javascript and the Discord.js package. Has a wide variety of commands and features for your server and members!",
+    description: "A Multi Purpose Discord Bot made using javascript and Discord.js. Has a wide variety of commands and features for your server and members!",
       image: '/images/1.png',
-      tags: ['Discord.js', 'Javascript', 'Mongo', 'Node'],
+      tags: ['Discord.js', 'Javascript', 'MongoDB', 'Node'],
     source: 'https://github.com/KingRain',
     visit: 'https://dogethebot.github.io',
     id: 0,
@@ -19,12 +19,21 @@ export const projects = [
   },
   {
     title: 'SpongeBob',
-    description:"SpongeBob is a Bot made in Discord.js whihc allows all users without nitro to use Nitro Emotes in a server!",
+    description:"SpongeBob is a Bot made in Discord.js which allows all users without nitro to use Nitro Emotes in a server!",
     image: '/images/3.png',
-    tags: ['Python', 'Discord.py'],
+    tags: ['Discord', 'Discord.js'],
     source: 'https://github.com/KingRain',
     visit: 'https://github.com/KingRain',
-    id: 1,
+    id: 2,
+  },
+  {
+    title: 'Discord WebScrapper',
+    description:"Discord WebScarpper is a program/script which scraps all the Text and Images from a Discord Server with the Help of an Account.",
+    image: '/images/4.png',
+    tags: ['Discord', 'Python', 'WebScarpping'],
+    source: 'https://github.com/KingRain/DiscordWebScrapper',
+    visit: 'https://github.com/KingRain',
+    id: 3,
   },
 ];
 
